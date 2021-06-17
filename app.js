aa67cc392fc9aae5784d79782fa51a67ea465e38
Sylvester /hoe <sylvesterhoe@gmail.com>
2021-06-17 21:09:53 +0800

    full text

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const createTypingDiv = () => {
 };
 
 const textGen = () => {
-  const texts = ["A Hamiltonian path", "Several algorithms for"];
+  const texts = ["A Hamiltonian path or traceable path is a path that visits each vertex of the graph exactly once. A graph that contains a Hamiltonian path is called a traceable graph. A graph is Hamiltonian-connected if for every pair of vertices there is a Hamiltonian path between the two vertices.", "Several algorithms for finding cycles quickly and with little memory are known. Robert W. Floyd's tortoise and hare algorithm moves two pointers at different speeds through the sequence of values until they both point to equal values. Alternatively, Brent's algorithm is based on the idea of exponential search. Both Floyd's and Brent's algorithms use only a constant number of memory cells, and take a number of function evaluations that is proportional to the distance from the start of the sequence to the first repetition. Several other algorithms trade off larger amounts of memory for fewer function evaluations."];
 
   let randText = texts[Math.floor(Math.random() * texts.length)];
   randText.split("").map((char) => {
